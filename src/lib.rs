d138383b1d155917ad6b5269989e8654b5ce5d31
Darren Meehan <hi@drn.ie>
2020-04-26 18:44:50 +0100

    Add link to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use web_sys::console;
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
-// If you don't want to use `wee_alloc`, you can safely delete this.
+// https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/template-deep-dive/wee_alloc.html#what-is-wee_alloc
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
