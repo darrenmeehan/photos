89150e408d87dc4657f10e2d5ebbded1a00117a6
Darren Meehan <hi@drn.ie>
2020-05-03 17:26:08 +0100

    Adding example component

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
+// use yew::prelude::*;;;;
+// use yew::{html};
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -19,9 +22,95 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
+    html! {
+        <>
+            <div></div>
+            <p></p>
+        </>
+    };
+
+    demo();
+
+    greet();
+
     Ok(())
+}
+
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, test-wasm!");
+}
+
+#[wasm_bindgen]
+pub fn get_photos() {
+    return create_demo_photos()
+}
+
+fn create_demo_photos() {
+    // Eventually get this information from DB.
+    // let photo_names = 
+    // let photo_names = [
+    //     "1.jpg"
+    // ];
+    let path = "samples/1.png";
+
+    // let first_photo = Photo { path };
+
+    // return photo_names
+}
+
+struct Photo {
+    // id: u64, // Is this needed?
+    path: str
+    // name, Have two values? Human readable, and url encoded?
+    // description
+}
+
+pub struct Model {
+    link: ComponentLink<Self>,
+}
+
+pub enum Msg {
+    Click,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Model { link }
+    }
+
+    fn change(&mut self, _: Self::Properties) -> bool {
+        false
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {}
+        }
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::Click)>{ "Click ( wasm-bindgen )" }</button>
+            </div>
+        }
+    }
+}
+
+fn demo() {
+    yew::start_app::<Model>();
 }
